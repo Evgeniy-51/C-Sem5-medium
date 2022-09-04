c1ebf2b427e9158e030d3ba1bc316caf3f506655
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-04 19:24:16 +0300

    task2

---
+++ task1/task1.cs
@@ -0,0 +1,40 @@
+﻿// // Задача 1 Реализовать алгоритм, который выводит на экран возрастающую
+// последовательность случайных чисел от 1 до 1000. Как только сумма цифр
+// очередного числа в последовательности достигнет 26, остановить генерацию,
+// вывести на экран сообщение «Stop».
+
+Console.Clear();
+
+int sumCounter(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        int dig = num % 10;
+        sum += dig;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Random rnd = new Random();
+int minLim = 1;
+int sum = 0;
+string line = "";
+
+while (minLim <= 1000)
+{
+    int num = rnd.Next(minLim, 1000);
+    line += num.ToString() + " ";
+    sum = sumCounter(num);
+
+    if (sum >= 26)
+    {
+        System.Console.WriteLine(line + "Stop");
+        minLim = 9999;
+    }
+    else
+    {
+        minLim = num + 1;
+    }
+}
---
+++ task2/task2.cs
@@ -0,0 +1,45 @@
+﻿// // Михаил интересуется созданием простых чисел. Его интересуют
+// простые числа с некоторыми характеристиками:
+// • число сгенерировано случайным образом;
+// • его квадрат не оканчивается на 1;
+// • сумма его цифр лежит в диапазоне от 10 до 20
+// Сгенерировать пять простых чисел, удовлетворяющих пожеланиям
+// Михаила, и вывести их на экран.
+
+Console.Clear();
+
+Random rnd = new Random();
+
+bool isPrime(int num)  // проверка на простое
+{
+    if (num == 2) return true;
+
+    for (int i = 2; i < Math.Sqrt(num); i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int createNum()  // создание числа
+{
+    int num = rnd.Next(2, 1000);
+    int tempNum = num;
+    int digSum = 0;
+
+    while (tempNum != 0)
+    {
+        int dig = tempNum % 10;
+        digSum += dig;
+        tempNum = tempNum / 10;
+    }
+    return (isPrime(num) && (num * num) % 10 != 1 && digSum >= 10 && digSum <= 20) ? num : createNum();
+}
+
+string res = "";
+for (int i = 0; i < 5; i++)
+{
+    res += createNum().ToString() + "  ";
+}
+
+Console.WriteLine(res);
