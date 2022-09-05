245784c0b0ff6b90c3eaec7a895a89703ee9c4bc
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-05 15:30:00 +0300

    task2

--- task2/task2.cs
+++ task2/task2.cs
@@ -15,7 +15,7 @@ bool isPrime(int num)  // проверка на простое
     if (num == 2) return true;
     if (num % 2 == 0) return false;
 
-    for (int i = 3; i < num / 3; i = i + 2)
+    for (int i = 3; i < Math.Round(Math.Sqrt(num)) + 1; i = i + 2)
     {
         if (num % i == 0) return false;
     }
